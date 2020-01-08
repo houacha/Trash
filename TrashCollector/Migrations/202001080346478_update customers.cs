6ea4667a7a03e6ba9ae9472e7254a0d774fd3766
houa cha <itshoua@gmail.com>
2020-01-07 23:11:03 -0600

    init but i added a bunch of codes and scaffolding did majority of my work

---
+++ TrashCollector/Migrations/202001080346478_update customers.cs
@@ -0,0 +1,26 @@
+﻿namespace TrashCollector.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatecustomers : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Zip", c => c.String());
+            AlterColumn("dbo.Customers", "PickupDay", c => c.String());
+            AlterColumn("dbo.Customers", "ExtraPickupDate", c => c.String());
+            AlterColumn("dbo.Customers", "SuspendedStart", c => c.String());
+            AlterColumn("dbo.Customers", "SuspendedEnd", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "SuspendedEnd", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Customers", "SuspendedStart", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Customers", "ExtraPickupDate", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Customers", "PickupDay", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Customers", "Zip", c => c.Int(nullable: false));
+        }
+    }
+}
---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Zip { get; set; }
+        public string Address { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public double Balance { get; set; }
+        [Display(Name = "Pick-up Day")]
+        public string PickupDay { get; set; }
+        [Display(Name = "Extra Pick-up Date")]
+        public string ExtraPickupDate { get; set; }
+        [Display(Name = "Suspended Start")]
+        public string SuspendedStart { get; set; }
+        [Display(Name = "Suspended End")]
+        public string SuspendedEnd { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
